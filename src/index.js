0b5c9757955a01befc9bf14c64ab553a32012b91
Andrés Carreño <andresdavid90@gmail.com>
2016-03-15 19:54:28 -0500

    Adding first version

---
+++ src/index.js
@@ -0,0 +1,38 @@
+#!/usr/bin/env node
+
+var stdio = require('stdio');
+var tumblrLksDownldr = require('./../../tumblr-lks-downldr/src/index.js');
+
+var args = stdio.getopt(
+  {
+    'url': {
+      key: 'u',
+      args: 1,
+      description: 'Valid Tumblr URL.',
+      mandatory: true
+    },
+    'postsToLoad': {
+      key: 'l',
+      args: 1,
+      description: 'Number of posts liked that you want to load and download.'
+    },
+    'path': {
+      key: 'p',
+      args: 1,
+      description: 'Relative path to save the images.'
+    }
+  }
+);
+
+tumblrLksDownldr.setGlobalParams(
+  {
+    url: args.url,
+    postsToLoad: args.postsToLoad,
+    path: args.path
+  }
+);
+
+console.log('Tumblr Blog:', args.url);
+console.log('Saving in:', args.path);
+
+tumblrLksDownldr.getLikedPosts();
